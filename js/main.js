87a3188e2724d373ba5f5b73cc10392444c81e6a
Jo Muncaster <j_muncaster208947@fanshaweonline.ca>
2025-07-29 08:48:38 -0400

    Update main.js
    In this commit, the main.js was updated to include a this statement to remove the highlighting when you dragleave an area.

--- js/main.js
+++ js/main.js
@@ -113,6 +113,9 @@ icons.forEach(icon => {
 targetZones.forEach(target => {
     target.addEventListener("dragover", dragOver);
     target.addEventListener("drop", drop);
+    target.addEventListener("dragleave", function () {
+        this.classList.remove("highlight");
+    });
 });
 
 
